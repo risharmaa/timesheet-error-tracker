7c15699c8bd251bccd57ac190abcaa6afafefdf4
Riya Sharma <riya@Riyas-MacBook-Air.local>
2026-05-28 12:49:30 -0400

    created database schema and added sample users/a timesheet

---
+++ schema.sql
@@ -0,0 +1,57 @@
+-- Active: 1779985622938@@127.0.0.1@3306@mysql
+
+CREATE DATABASE IF NOT EXISTS project
+    DEFAULT CHARACTER SET = 'utf8mb4';
+USE project;
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS people;
+CREATE TABLE people (
+  userid    varchar(254) not null,
+  fname     varchar(30),
+  lname     varchar(30),
+  type      varchar(10),
+  primary key (userid)
+);
+
+DROP TABLE IF EXISTS clients;
+CREATE TABLE clients (
+  clientid      varchar(254),
+  caregiverid   varchar(254),
+  primary key (clientid, caregiverid),
+  foreign key (clientid) references people(userid),
+  foreign key (caregiverid) references people(userid)
+);
+
+DROP TABLE IF EXISTS admin;
+CREATE TABLE admin (
+  adminid     varchar(254),
+  password    varchar(20)
+);
+
+DROP TABLE IF EXISTS timesheet;
+CREATE TABLE timesheet (
+  timesheetid   INTEGER PRIMARY KEY AUTO_INCREMENT,
+  clientid      varchar(254),
+  caregiverid   varchar(254),
+  date          DATE,
+  reason        varchar(50),
+  sent          BOOLEAN DEFAULT FALSE,
+  received      BOOLEAN DEFAULT FALSE,
+  type          varchar(50),
+  foreign key (clientid) references people(userid),
+  foreign key (caregiverid) references people(userid)
+);
+
+
+-- adding a test admin (for logging in)
+INSERT INTO people VALUES ('admin@gmail.com', 'Test', 'Admin', 'Admin');
+INSERT INTO admin VALUES ('admin@gmail.com', 'pass');
+
+-- adding a test client and 2 caregivers
+INSERT INTO people VALUES ('client@gmail.com', 'Test', 'Client', 'Client');
+INSERT INTO people VALUES ('caregiver1@gmail.com', 'Test1', 'Caregiver', 'Caregiver');
+INSERT INTO people VALUES ('caregiver2@gmail.com', 'Test2', 'Caregiver', 'Caregiver');
+INSERT INTO clients VALUES ('client@gmail.com', 'caregiver1@gmail.com');
+INSERT INTO clients VALUES ('client@gmail.com', 'caregiver2@gmail.com');
+--adding a timesheet error
+INSERT INTO timesheet VALUES (1, 'client@gmail.com', 'caregiver1@gmail.com', '2026-05-28', 'Out of Window Clock-out', FALSE, FALSE, NULL);
